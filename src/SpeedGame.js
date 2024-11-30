d84fd60792a92bb483f5eedad3a82dab630bc42d
Oleksii Shpilov <chrismasroom@gmail.com>
2024-12-01 00:46:41 +0200

    change to actual 10%

--- src/SpeedGame.js
+++ src/SpeedGame.js
@@ -3,6 +3,6 @@ import { BaseGame } from "./BaseGame";
 export default class SpeedGame extends BaseGame {
     _addFood() {
         super._addFood()
-        this._updateTimeMS *= 0.9
+        this._updateTimeMS *= 1 / 1.1
     }
 }
